4a55fe41d5a858730b48eb79036026f772c4bbd2
Anurag Dangi <anuragdangi015@gmail.com>
2025-01-08 18:34:00 +0530

    Middleware concept

--- src/app.js
+++ src/app.js
@@ -2,41 +2,24 @@ const express = require('express');
 
 const app = express();
 
-// app.get('/user', (req, res) => {
-//   res.send({ firstname: 'Anurag', lastname: 'Singh' });
-// });
-
-// app.post('/user', (req, res) => {
-//   //data saved in database
-//   res.send('User created successfully in the database');
-// });
-
-// app.delete('/user', (req, res) => {
-//   res.send('Delete successfully');
-// });
-
-app.use(
-  '/test',
-  (req, res, next) => {
-    console.log('this is the 1st middleware');
+app.use('/admin', (req, res, next) => {
+  //Logic to check if the requesr is authorized
+  const token = 'xyz';
+  const isAuthorizedAdmin = token === 'xyz';
+  if (!isAuthorizedAdmin) {
+    res.status(401).send('You are not authorized to see this data');
+  } else {
     next();
-    // res.send('this is the 1st response');
-  },
-  (req, res, next) => {
-    console.log('this is the 2nd middleware');
-    // res.send('this is the 2nd response');
-    next();
-  },
-  (req, res, next) => {
-    console.log('this is the 3rd middleware');
-    // res.send('this is the 3rd response');
-    next();
-  },
-  (req, res) => {
-    console.log('this is the 4th middleware');
-    res.send('this is the 4th response');
   }
-);
+});
+
+app.get('/admin/getAlldata', (req, res) => {
+  res.send('Here is the data that only admin can see');
+});
+
+app.get('/admin/deleteUser', (req, res) => {
+  res.send('User deleted successfully');
+});
 
 app.listen(7777, () => {
   console.log('listening on port 7777');
